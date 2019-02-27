8a1ce278e02ca3ee5c00ee914f9e0f0bdb8077a9
phi <phi@MacBook-Air-5.local>
2019-02-27 12:56:38 +0100

    Change2

--- src/app/build/build.component.ts
+++ src/app/build/build.component.ts
@@ -10,8 +10,51 @@ export class BuildComponent implements OnInit {
 
   constructor(private service: BuildService) { }
 
+  models:Array<any>
   ngOnInit() {
+
+    this.getModels()
+    this.models=[]
 
   }
 
+  getModels(){
+
+    this.service.getAllModels().subscribe(
+      result => {
+        result = JSON.parse(result);
+        for (let info of result){
+          
+            for (let node of info.nodes){
+              
+              this.service.getModelInfo(info.text,node.text).subscribe(
+                result=>{
+                    let trained=false
+                    if (result){
+                      trained=true 
+                    }
+                   
+                    this.models.push({name:info.text,version:node.text,trained:trained})
+                },
+                error =>{
+                  alert("Error getALL each model")
+                }
+              );
+            }
+        }
+        
+    },
+    error => {
+        alert("Error getALL models")
+    }
+
+    );
+    console.log(this.models)
+  }
+  openModel(){
+    alert("Open model windows to VIEW model")
+  }
+  newModel(){
+    alert("Open model windows to NEW model")
+  }
 }
