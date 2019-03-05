846e55116d32ded5c8a546facdcfacf438f3d1d0
IgnacioPasamontes <ignacio.pasamontes@upf.edu>
2019-03-05 16:56:51 +0100

    Change5

--- src/app/newmodel/newmodel.component.ts
+++ src/app/newmodel/newmodel.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit, ComponentRef } from '@angular/core';
-import {IModalDialog, IModalDialogOptions, IModalDialogButton} from 'ngx-modal-dialog'
-import {NewmodelService} from './newmodel.service'
+import { Component, OnInit, ComponentRef, ViewChild, ElementRef, OnChanges } from '@angular/core';
+import {IModalDialog, IModalDialogOptions, IModalDialogButton} from 'ngx-modal-dialog';
+import {NewmodelService} from './newmodel.service';
 
 @Component({
   selector: 'app-newmodel',
   templateUrl: './newmodel.component.html',
   styleUrls: ['./newmodel.component.css']
 })
-export class NewmodelComponent implements OnInit, IModalDialog {
+export class NewmodelComponent implements OnInit, IModalDialog, OnChanges {
 
+  @ViewChild('trainigseries') trainserieElement: ElementRef;
   actionButtons: IModalDialogButton[];
-  modelName:string="";
+  modelName = '';
+  trainigseriesEnable = true;
+  modelEnable = true;
+  validationEnable = true;
+  trainigseriesCollapsed = true;
 
   constructor(private service: NewmodelService) {
     this.actionButtons = [
@@ -28,34 +33,52 @@ export class NewmodelComponent implements OnInit, IModalDialog {
 
   }
 
- 
-    /**
-     * Creates a new model with the given name and informs the user with a toastr
-     */
-    createModel(): void {
-      const letters = /^[A-Za-z0-9_]+$/;
-      alert(this.modelName)
-      if (this.modelName.match(letters)) {
-          this.service.createNewModel(this.modelName).subscribe(
-              result => {
-                  if (result[0] === true) {
-                      alert("Created")
-                  } else {
-                      alert("ERROR1")
-                  }
-              },
-              error => {
-                 alert("ERROR2")
-              },
-              () => {
-                 alert("no se que ha pasado")
-              }
-          );
-      } else {
-          alert("Invalid name");
-      }
-  
-
+  ngOnChanges() {
+    alert('Changes');
   }
 
+  /**
+   * Creates a new model with the given name and informs the user with a toastr
+   */
+  createModel(): void {
+    const letters = /^[A-Za-z0-9_]+$/;
+    if (this.modelName.match(letters)) {
+        this.service.createNewModel(this.modelName).subscribe(
+            result => {
+                if (result[0] === true) {
+                    this.trainigseriesEnable = false;
+                    this.modelEnable = false;
+                    this.validationEnable = false;
+                    this.trainserieElement.nativeElement.className = 'collapse show';
+                } else {
+                    //alert('ERROR1');
+                }
+            },
+            error => {
+                //alert('ERROR2');
+            },
+            () => {
+                //alert('no se que ha pasado');
+                this.getParameters();
+            }
+        );
+    } else {
+        alert('Invalid name');
+    }
+  }
+
+  getParameters(): void{
+    this.service.getParameters(this.modelName).subscribe(
+      result => {
+       console.log(JSON.parse(result));
+      },
+      error => {
+        console.log(error);
+      },
+      () => { // when subscribe finishes
+        // console.log('actual parameters.yaml \n', parameters);
+        alert('Hola');
+      }
+    );
+  }
 }
