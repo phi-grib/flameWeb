59ce6e07797727863720b8275e21f0271eae27f6
Ignacio Pasamontes <36951846+IgnacioPasamontes@users.noreply.github.com>
2019-03-08 15:20:14 +0100

    Change upload file

--- src/app/training-series/training-series.component.ts
+++ src/app/training-series/training-series.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-//import { SDFService } from './sdf.service';
-import { FileUploader, FileSelectDirective } from 'ng2-file-upload';
+// import { SDFService } from './sdf.service';
+import { FileUploader, FileSelectDirective, FileItem } from 'ng2-file-upload';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { Model } from '../Model';
@@ -16,7 +16,7 @@ export class TrainingSeriesComponent implements OnInit {
 
   constructor(
     private http: HttpClient,
-    //private molService: SDFService,
+    // private molService: SDFService,
     private model: Model,
     private router: Router,
   ) { }
@@ -25,40 +25,50 @@ export class TrainingSeriesComponent implements OnInit {
     url: this.url,
     itemAlias: 'file',
   });
+
   /**
    * Checks if the first step is completed and initites the upload method observable
    */
   ngOnInit(): void {
     this.uploadFile();
+    this.uploader.onBeforeUploadItem = (fileItem: any) => {
+      const extension = fileItem.file.name.split('.');
+      fileItem.file.name = this.model.name + '_' + this.model.version + '.' + extension[1];
+      console.log('onCompleteItem');
+      console.log(fileItem);
+      console.log('--------');
+    };
+    /*this.uploader.onAfterAddingFile = (file) => {
+      console.log('***** onAfterAddingFile ******');
+      console.log('file ', file.file.name);
+      file.file.name = 'nacho';
+    };*/
   }
-  onFileChanged(event) {
-    console.log( event.target.files[0].name);
-   
-   }
-   
   /**
    * Uploads the sdf file to the server and uses a toastr to informs the user
    */
   uploadFile(): void {
-    
     this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
     this.uploader.onSuccessItem = (item: any, response: any, status: any, headers: any) => {
       if (response !== '0') {
-        //this.toastr.success('SDF uploaded correctly', 'Success!', { closeButton: true, positionClass: 'toast-bottom-right' });
+        //  this.toastr.success('SDF uploaded correctly', 'Success!', { closeButton: true, positionClass: 'toast-bottom-right' });
         const data = JSON.parse(response);
+        console.log('####################');
+        console.log(data);
+        console.log('####################');
         if (data) {
           this.model.molNumber = data[0];
           this.model.fileFields = Object.keys(data[1]);
           this.model.fileData = data[1];
           this.model.file = data[2];
         }
-        console.log(this.model)
+        console.log(this.model);
       } else {
-        //this.toastr.error('Invalid file', 'Error', { closeButton: true, positionClass: 'toast-bottom-right' });
+        // this.toastr.error('Invalid file', 'Error', { closeButton: true, positionClass: 'toast-bottom-right' });
       }
     };
     this.uploader.onErrorItem = (item: any, response: any, status: any, headers: any) => {
-      //this.toastr.error('File upload failed', 'Error');
+      // this.toastr.error('File upload failed', 'Error');
     };
   }
 
